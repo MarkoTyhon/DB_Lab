83b95f5c91e101eb225369ab0cc7ee5ab4f185d1
marko <marko.tyhon.2004@gmail.com>
2023-04-27 22:41:31 +0300

    temp

--- Interface/main.py
+++ Interface/main.py
@@ -80,7 +80,7 @@ insert_button.pack()
 
 # Start the Tkinter event loop
 root.mainloop()"""
-    
+""" 
 import mysql.connector
 from tkinter import *
 from tkinter import messagebox
@@ -137,4 +137,127 @@ show_button.pack()
 
 # Start the Tkinter event loop
 root.mainloop()
- 
+"""
+
+
+import mysql.connector
+from tkinter import *
+from tkinter import messagebox
+
+# Connect to the database
+# Connect to the database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="qwertz123",
+    database="computer_components"
+    )
+
+# Create a cursor object
+mycursor = mydb.cursor()
+
+# Create the main Tkinter window
+root = Tk()
+root.title("Computer Components Database")
+root.geometry("800x600")
+
+"""
+def show_table(table_name):
+    # Clear the components listbox
+    components_listbox.delete(0, END)
+
+    # Retrieve data from the specified table
+    mycursor.execute(f"SELECT * FROM {table_name}")
+
+    # Fetch all the rows
+    data = mycursor.fetchall()
+
+    # Insert the data into the listbox
+    for row in data:
+        components_listbox.insert(END, row)
+
+
+# Create a Frame for the components display
+components_frame = Frame(root)
+components_frame.pack(pady=20)
+
+# Create a scrollbar for the components display
+scrollbar = Scrollbar(components_frame)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+# Create a Listbox to display the components
+components_listbox = Listbox(components_frame, width=100, yscrollcommand=scrollbar.set)
+components_listbox.pack(side=LEFT, fill=BOTH)
+scrollbar.config(command=components_listbox.yview)
+
+
+# Function to handle button click events
+def button_click(table_name):
+    messagebox.showinfo("Button Clicked", f"Displaying {table_name}")
+    show_table(table_name)
+
+
+# Create buttons for each table
+tables = ["components", "manufacturers", "reviews", "orders", "order_items", "component_manufacturer"]
+
+for table_name in tables:
+    button = Button(root, text=table_name.capitalize(), command=lambda name=table_name: button_click(name))
+    button.pack(pady=5)
+
+
+# Start the Tkinter event loop
+root.mainloop()
+
+"""
+
+def show_table(table_name):
+    # Clear the components listbox
+    components_listbox.delete(0, END)
+
+    # Retrieve column names from the specified table
+    mycursor.execute(f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'")
+    columns = mycursor.fetchall()
+    column_names = [column[0] for column in columns]
+
+    # Retrieve data from the specified table
+    mycursor.execute(f"SELECT * FROM {table_name}")
+    data = mycursor.fetchall()
+
+    # Insert the column names into the listbox
+    components_listbox.insert(END, " | ".join(column_names))
+
+    # Insert the data into the listbox
+    for row in data:
+        components_listbox.insert(END, " | ".join(str(val) for val in row))
+
+
+# Create a Frame for the components display
+components_frame = Frame(root)
+components_frame.pack(pady=20)
+
+# Create a scrollbar for the components display
+scrollbar = Scrollbar(components_frame)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+# Create a Listbox to display the components
+components_listbox = Listbox(components_frame, width=100, yscrollcommand=scrollbar.set)
+components_listbox.pack(side=LEFT, fill=BOTH)
+scrollbar.config(command=components_listbox.yview)
+
+
+# Function to handle button click events
+def button_click(table_name):
+    messagebox.showinfo("Button Clicked", f"Displaying {table_name}")
+    show_table(table_name)
+
+
+# Create buttons for each table
+tables = ["components", "manufacturers", "reviews", "orders", "order_items", "component_manufacturer"]
+
+for table_name in tables:
+    button = Button(root, text=table_name.capitalize(), command=lambda name=table_name: button_click(name))
+    button.pack(pady=5)
+
+
+# Start the Tkinter event loop
+root.mainloop()
