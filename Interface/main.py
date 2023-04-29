a479d99ed2702b646b9778648017b5212867d2c7
marko <marko.tyhon.2004@gmail.com>
2023-04-30 01:14:01 +0300

    Final Interface

--- Interface/main.py
+++ Interface/main.py
@@ -1,117 +1,58 @@
-import mysql.connector
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
+import mysql.connector
+
 # Connect to the database
-# Connect to the database
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="qwertz123",
+conn = mysql.connector.connect(
+    host="<host>", #your_host
+    user="<user>", #your_user
+    password="<password>", #your_password
     database="computer_components"
     )
 
-# Create a cursor object
-mycursor = mydb.cursor()
+# Create a cursor object to interact with the database
+cursor = conn.cursor()
 
-# Create the main Tkinter window
-root = Tk()
+# Create the main application window
+root = tk.Tk()
 root.title("Computer Components Database")
-root.geometry("1920x1000")
 
-
-
+# Function to handle button clicks for each table
 def show_table(table_name):
-    # Clear the components treeview
-    components_treeview.delete(*components_treeview.get_children())
+    # Execute a SELECT query to retrieve data from the table
+    cursor.execute(f"SELECT * FROM {table_name}")
+    data = cursor.fetchall()
 
-    # Retrieve column names from the specified table
-    mycursor.execute(f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'")
-    columns = mycursor.fetchall()
-    column_names = [column[0] for column in columns]
+    # Create a new window to display the table data
+    table_window = tk.Toplevel(root)
+    table_window.title(table_name)
 
-    # Retrieve data from the specified table
-    mycursor.execute(f"SELECT * FROM {table_name}")
-    data = mycursor.fetchall()
+    # Create a treeview widget to display the data
+    tree = ttk.Treeview(table_window)
 
-    # Insert the column names into the treeview
-    components_treeview['columns'] = column_names
-    for col in column_names:
-        components_treeview.heading(col, text=col)
+    # Define the table columns
+    tree["columns"] = tuple(cursor.column_names)
 
-    # Insert the data into the treeview
+    # Configure column headings
+    for column in cursor.column_names:
+        tree.heading(column, text=column)
+
+    # Insert data into the treeview
     for row in data:
-        components_treeview.insert('', END, values=row)
+        tree.insert("", tk.END, values=row)
 
-
-def insert_data():
-    table_name = table_combobox.get()
-    values = []
-    for entry in entry_list:
-        values.append(entry.get())
-
-    # Insert data into the selected table
-    try:
-        query = f"INSERT INTO {table_name} VALUES {tuple(values)}"
-        mycursor.execute(query)
-        mydb.commit()
-        messagebox.showinfo("Insert Successful", "Data inserted successfully!")
-    except Exception as e:
-        messagebox.showerror("Insert Error", str(e))
-
-    # Refresh the displayed table
-    show_table(table_name)
-
-
-# Create a Frame for the components display
-components_frame = Frame(root)
-components_frame.pack(pady=20)
-
-# Create a Treeview to display the components
-components_treeview = ttk.Treeview(components_frame)
-components_treeview.pack()
-
-# Create a Frame for the insert form
-insert_frame = Frame(root)
-insert_frame.pack(pady=10)
-
-# Create a Combobox for table selection
-table_combobox = ttk.Combobox(insert_frame, values=("components", "manufacturers", "reviews", "orders", "order_items", "component_manufacturer"))
-table_combobox.grid(row=0, column=0, padx=5)
-
-# Create an empty list to store the Entry widgets
-entry_list = []
-
-# Retrieve column names from the first table (components) for the insert form
-mycursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'components'")
-columns = mycursor.fetchall()
-column_names = [column[0] for column in columns]
-
-# Create Entry widgets for each column in the insert form
-for i, col in enumerate(column_names):
-    label = Label(insert_frame, text=col)
-    label.grid(row=i+1, column=0, padx=5, pady=5)
-    entry = Entry(insert_frame)
-    entry.grid(row=i+1, column=1, padx=5, pady=5)
-    entry_list.append(entry)
-
-# Create a Button to insert data into the selected table
-insert_button = Button(insert_frame, text="Insert", command=insert_data)
-insert_button.grid(row=len(column_names)+1, columnspan=2, padx=5, pady=10)
-
-
-def button_click(table_name):
-    messagebox.showinfo("Button Clicked", f"Displaying {table_name}")
-    show_table(table_name)
-
+    # Pack the treeview into the window
+    tree.pack()
 
 # Create buttons for each table
 tables = ["components", "manufacturers", "reviews", "orders", "order_items", "component_manufacturer"]
+for table in tables:
+    btn = tk.Button(root, text=table.capitalize(), command=lambda t=table: show_table(t))
+    btn.pack(pady=5)
 
-for i, table_name in enumerate(tables):
-    button = ttk.Button(root, text=table_name.capitalize(), command=lambda name=table_name: button_click(name))
-    button.pack(pady=5)
-
-
-# Start the Tkinter event loop
+# Run the main event loop
 root.mainloop()
+
+# Close the cursor and the database connection
+cursor.close()
+conn.close()
